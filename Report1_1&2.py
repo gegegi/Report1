c500b1b248a576afc9f1194a2779ece978cf3210
gegegi <tjzl1995@kongju.ac.kr>
2015-04-23 00:20:15 +0900

    - 1-(1) Inverse triangular matrix TODO : LU Facto

--- Report1_1&2.py
+++ Report1_1&2.py
@@ -3,21 +3,51 @@ __author__ = 'seokhui'
 from numpy import matrix
 from numpy import linalg
 
-A1 = matrix([[2, 4, -2], [4, 9, -3], [-2, -3, 7]])
-b1 = matrix([[2], [8], [10]])
+A1 = [[2, 4, -2], [4, 9, -3], [-2, -3, 7]]
+b1 = [[2], [8], [10]]
 
-A2 = matrix([[1, 2], [4, 9]])
-b2 = matrix([[5], [9]])
+A2 = [[1, 2], [4, 9]]
+b2 = [[5], [9]]
+
+# 1-(1)
+def by_gaussian_elimination(a, b):
+    row = int(len(a[0]))
+    pivots = []
+
+    j = 0
+    for p in range(row):
+        i = 0
+        while True:
+            if not(a[p][j] == 0):
+                break
+            temp = a[p]
+            i += 1
+            i = int((i+1) % row)
+            a[p] = a[i]
+            a[i] = temp
+
+        pivots.append(a[p][j])
+
+        for r in range(p+1, row):
+            for temp in range(row):
+                a[r][temp] = a[r][temp] - (a[r][p]/pivots[p])*a[p][temp]
+
+        j += 1
+        j = int(j % row)
+
+    print("y = "+str(b[2][0]/a[2][2]))
 
 
-pivots1 = range(A1.size)
+by_gaussian_elimination(A1, b1)
 
-
-def by_inverse_matrix(a, b):
-    # (Matrix variable).I means (Matrix variable)'s Inverse matrix
+# 1-(2)
+def by_inverse_matrix(l, b):
+    a = matrix(l)
+    # (Matrix variable).I means (Matrix variable)'s Inverse matrix - numpy.matrix
     inverse = a.I
-    x = inverse*b
+    x = inverse * b
     print(x)
+
 
 print("1.2 - (1) output by Inverse Matrix :")
 by_inverse_matrix(A1, b1)
