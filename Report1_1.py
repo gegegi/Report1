1bf4b7a13eb7079afe961fb50a3fe104a1805dee
이석희 <tjzl1995@gmail.com>
2015-04-24 16:33:35 +0900

    Report1_1 fix

--- Report1_1.py
+++ Report1_1.py
@@ -1,7 +1,5 @@
 __author__ = 'seokhui_lee'
 
-from numpy import matrix
-
 A1 = [[2, 4, -2], [4, 9, -3], [-2, -3, 7]]
 b1 = [[2], [8], [10]]
 
@@ -17,33 +15,51 @@ def by_gaussian_elimination(a, b):
     for p in range(row):
         i = 0
         while True:
-            if not(a[p][j] == 0):
+            if not (a[p][j] == 0):
                 break
             temp = a[p]
             i += 1
-            i = int((i+1) % row)
+            i = int((i + 1) % row)
             a[p] = a[i]
             a[i] = temp
 
         pivots.append(a[p][j])
 
-        for r in range(p+1, row):
-            multiplier = a[r][p]/pivots[p]
-            b[r][0] = b[r][0] - multiplier*b[p][0]
+        for r in range(p + 1, row):
+            multiplier = a[r][p] / pivots[p]
+            b[r][0] = b[r][0] - multiplier * b[p][0]
             for temp in range(row):
-                a[r][temp] = a[r][temp] - multiplier*a[p][temp]
+                a[r][temp] = a[r][temp] - multiplier * a[p][temp]
 
         j += 1
         j = int(j % row)
 
-    print(A1)
-    print(b1)
+    print()
+    print(b)
 
+
+print("1.1 - (1) output by Inverse Matrix :")
 by_gaussian_elimination(A1, b1)
 
+print("1.1 - (2) output by Inverse Matrix :")
+by_gaussian_elimination(A2, b2)
 
-def gausselim1(themat):
-   for i in range(len(themat[0])):
-      for j in range(i+1,len(themat)):
-         m = themat[j][i]/themat[i][i]   # Ratio of (i,j) elt by (i,i) (diagonal) elt
-         themat[j] = [themat[j][k]-m*themat[i][k] for k in range(len(themat[0]))]
+A1 = [[2, 4, -2], [4, 9, -3], [-2, -3, 7]]
+b1 = [[2], [8], [10]]
+
+A2 = [[1, 2], [4, 9]]
+b2 = [[5], [9]]
+
+
+def gausselim_by_ref(themat):
+    for i in range(len(themat[0])):
+        for j in range(i + 1, len(themat)):
+            m = themat[j][i] / themat[i][i]  # Ratio of (i,j) elt by (i,i) (diagonal) elt
+            themat[j] = [themat[j][k] - m * themat[i][k] for k in range(len(themat[0]))]
+    print(themat)
+
+
+print("1.1 - (1) output by Inverse Matrix :")
+gausselim_by_ref(A1)
+print("1.1 - (2) output by Inverse Matrix :")
+gausselim_by_ref(A2)
